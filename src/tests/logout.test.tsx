e48be524fb95bb443c390c07eee171620df26a5d
Khaleb <khope7@hotmail.com>
2025-06-17 01:43:54 +0200

    added firebase mock to test component

--- src/tests/logout.test.tsx
+++ src/tests/logout.test.tsx
@@ -1,6 +1,16 @@
 import { render } from '@testing-library/react'
 import Logout from '../pages/RegisterPages/Logout'
 
+
+
+jest.mock('../../lib/firebase/firebaseConfig', () => ({
+auth: {
+currentUser: { email: 'test@example.com' },
+signOut: jest.fn(),
+},
+db: {},
+}));
+
 test ('matches snapshot', () => {
     const {asFragment} = render(<Logout />);
 
